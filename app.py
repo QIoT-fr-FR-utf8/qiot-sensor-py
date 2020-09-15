98ce3df35d8727f9543aaa4c90636cdd9550f256
Rémi VERCHERE <remi.verchere@axians.com>
2020-09-15 17:22:07 +0200

    Using prometheus
    Signed-off-by: Rémi VERCHERE <remi.verchere@axians.com>

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask,jsonify,request,render_template
 import enviroplus
 
-app = Flask(__name__)
+from prometheus_client import start_http_server, Summary
+import random
+import time
 
-@app.route('/')
-def home():
-  return "Hello from Flask"
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-#get /gas
-@app.route('/gas')
-def get_stores():
-  return jsonify({'gas': "mygas"})
-  #pass
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
 
-
-app.run(port=5000)
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        process_request(random.random())
