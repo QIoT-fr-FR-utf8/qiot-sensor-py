c0423c1909b19b7486b066909a14a4a898e780ad
Davidffry <david.auffray@axians.com>
2020-09-16 16:14:58 +0200

    correct particules

--- particules_extend.py
+++ particules_extend.py
@@ -6,30 +6,36 @@ data = pms5003.read()
 d_value_ug_m3={}
 d_value_per_1l={}
 
-d_value_ug_m3={"PM1_0":{"value":1.0,"atm":False}}
-d_value_ug_m3={"PM2_5":{"value":2.5,"atm":False}}
-d_value_ug_m3={"PM10":{"value":10,"atm":False}}
-d_value_ug_m3={"PM1_0_atm":{"value":1.0,"atm":True}}
-d_value_ug_m3={"PM2_5_atm":{"value":2.5,"atm":True}}
-d_value_ug_m3={"PM10_atm":{"value":10,"atm":True}}
+d_value_ug_m3.update({"PM1_0":{"value":1.0,"atm":False}})
+d_value_ug_m3.update({"PM2_5":{"value":2.5,"atm":False}})
+d_value_ug_m3.update({"PM10":{"atm":False}})
+d_value_ug_m3.update({"PM1_0_atm":{"value":1.0,"atm":True}})
+d_value_ug_m3.update({"PM2_5_atm":{"value":2.5,"atm":True}})
+d_value_ug_m3.update({"PM10_atm":{"value":10,"atm":True}})
 
-d_value_per_1l={"gt0_3um":{"value":0.3}}
-d_value_per_1l={"gt0_5um":{"value":0.5}}
-d_value_per_1l={"gt1_0um":{"value":1.0}}
-d_value_per_1l={"gt2_5um":{"value":2.5}}
-d_value_per_1l={"gt5_0um":{"value":5}}
-d_value_per_1l={"gt10um":{"value":10}}
+d_value_per_1l.update({"gt0_3um":{"value":0.3}})
+d_value_per_1l.update({"gt0_5um":{"value":0.5}})
+d_value_per_1l.update({"gt1_0um":{"value":1.0}})
+d_value_per_1l.update({"gt2_5um":{"value":2.5}})
+d_value_per_1l.update({"gt5_0um":{"value":5}})
+d_value_per_1l.update({"gt10um":{"value":10}})
 
 def r_int_value_pm_ug_per_m3(val,atm=False):
-    if isinstance(data.pm_ug_per_m3(val,atm), int):
-        return data.pm_ug_per_m3(val,atm)
-    else:
+    try:
+        if isinstance(data.pm_ug_per_m3(val,atm), int):
+            return data.pm_ug_per_m3(val,atm)
+        else:
+            return 0
+    except:
         return 0
 
 def r_int_value_pm_per_1l_air(val):
-    if isinstance(data.pm_ug_per_m3(val), int):
-        return data.pm_per_1l_air(val)
-    else:
+    try: 
+        if isinstance(data.pm_ug_per_m3(val), int):
+            return data.pm_per_1l_air(val)
+        else:
+            return 0
+    except :
         return 0
 
 
@@ -40,6 +46,6 @@ def json_parsing_return():
         d_jsonexport[k]=r_int_value_pm_ug_per_m3(v['value'],v['atm'])
 
     for k,v in d_value_per_1l.items()
-        d_jsonexport[k]=r_int_value_pm_ug_per_m3(v['value'],v['atm'])
+        d_jsonexport[k]=r_int_value_pm_per_1l_air(v['value'])
 
     return d_jsonexport
