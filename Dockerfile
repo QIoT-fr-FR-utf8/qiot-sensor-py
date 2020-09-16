02744e48811577912d577eeb194456783d36dc29
Davidffry <david.auffray@axians.com>
2020-09-16 22:12:46 +0200

    Test Fastapi

--- Dockerfile
+++ Dockerfile
@@ -10,12 +10,12 @@ WORKDIR /usr/src/app
 COPY requirements.txt ./
 
 ENV FLASK_APP /usr/src/app/app.py
-ENV FLASK_APP_PORT 8000
-ENV FLASK_APP_HOST "0.0.0.0"
-ENV FLASK_APP_DEBUG "False"
+ENV APP_PORT 8000
+ENV APP_HOST "0.0.0.0"
+ENV APP_DEBUG "False"
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python3", "app.py" ]
+CMD [ "uvicorn", "app:app","--host","$APP_HOST","--port","$APP_PORT" ]
--- app.py
+++ app.py
@@ -4,33 +4,52 @@
 import time
 import os
 
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+
 # Import flask for webservice and prometheus for metrics
-from flask import Flask,jsonify,Response,request
+# from flask import Flask,jsonify,Response,request
+
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
-# Import applications libs
+# # Import applications libs
 from metrics import REQUEST_TIME
 import gas_extend
 import particules_extend
 import weather_extend
 
 
+@app.get("/")
+def index():
+    return {"message": "Hello QIoT Teams"}
 # Create Flask application
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/metrics')
+@app.get('/metrics')
 def metrics():
-    """Flask endpoint to gather the metrics, will be called by Prometheus."""
+#     """Flask endpoint to gather the metrics, will be called by Prometheus."""
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
-@app.route('/')
-def index():
-    result={}
-    result['result']="Hello QIoT"
-    return jsonify(result)
+# @app.route('/')
+# def index():
+#     result={}
+#     result['result']="Hello QIoT"
+#     return jsonify(result)
 
+# @app.route('/api/sensors')
+# def listsensor():
+#     listofSensor=[]
+#     listofSensor.append('/api/sensors/gas')
+#     listofSensor.append('/api/sensors/pollution')
+#     listofSensor.append('/api/sensors/weather')
+#     result={}
+#     result={"result":listofSensor}
 
-@app.route('/api/sensors')
+#     return jsonify(result)
+
+@app.get('/api/sensors')
 def listsensor():
     listofSensor=[]
     listofSensor.append('/api/sensors/gas')
@@ -39,38 +58,28 @@ def listsensor():
     result={}
     result={"result":listofSensor}
 
-    return jsonify(result)
+    return jsonable_encoder(result)
 
-#get /api/gas
-# Message type
-# {
-#   "stationId":int,
-#   "instant":String*,
-#   "adc":double,
-#   "nh3":double,
-#   "oxidising":double,
-#   "reducing":double,
-# }
-
-@app.route('/api/sensors/gas', methods=['GET'])
+@app.get('/api/sensors/gas')
 def get_data_gas():
     result={}
     result={"result":gas_extend.json_parsing_return()}
-    return jsonify(result)
+    return jsonable_encoder(result)
 
-@app.route('/api/sensors/particules', methods=['GET'])
+@app.get('/api/sensors/particules')
 def get_data_particules():
     result={}
     result={"result":particules_extend.json_parsing_return()}
-    return jsonify(result)
+    return jsonable_encoder(result)
 
-@app.route('/api/sensors/weather', methods=['GET'])
+@app.get('/api/sensors/weather')
 @REQUEST_TIME.time()
 def get_weather():
     result={}
     result={"result":weather_extend.json_parsing_return()}
-    return jsonify(result)
+    return jsonable_encoder(result)
 
-if __name__=='__main__':
-    app.run(host=os.getenv('FLASK_APP_HOST'),port=os.getenv('FLASK_APP_PORT'),
-            debug=os.getenv('FLASK_APP_DEBUG'))
+# if __name__=='__main__':
+#     app.run(host=os.getenv('FLASK_APP_HOST'),port=os.getenv('FLASK_APP_PORT'),
+#             debug=os.getenv('FLASK_APP_DEBUG'))
+
