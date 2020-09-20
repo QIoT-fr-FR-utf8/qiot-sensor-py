9142cd9c4518a3bf007b3522ba2ecddedc4e70f1
Rémi Verchère <remi@verchere.fr>
2020-09-20 11:06:13 +0200

    try to fix LCD
    Signed-off-by: Rémi Verchère <remi@verchere.fr>

--- lcd.py
+++ lcd.py
@@ -36,9 +36,13 @@ size_x, size_y = draw.textsize(message, font)
 x = (WIDTH - size_x) / 2
 y = (HEIGHT / 2) - (size_y / 2)
 
+# Draw background rectangle and write text.
+draw.rectangle((0, 0, 160, 80), back_colour)
+draw.text((x, y), message, font=font, fill=text_colour)
+disp.display(img)
+
 def draw_message(display_message="[QIoT Axians]"):
     try: 
-        disp.begin()
         draw.rectangle((0, 0, 160, 80), back_colour)
         draw.text((x, y), display_message, font=font, fill=text_colour)
         disp.display(img)
